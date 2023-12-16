dde08820aad80569b053fbc08222351cf091e032
Abdullah Yakub <abdullahuraimov66@gmail.com>
2023-12-16 16:07:31 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title('Suv hayvonlarini klassifikatsiya qiluvchi model')
+
+#rasmni yuklash
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+
+    img = PILImage.create(file)
+
+    model = load_learner('fish_model.pkl')
+
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+    fig=px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
+    
